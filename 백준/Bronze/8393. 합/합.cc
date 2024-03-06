5c3229e992a214dcc1d28587bb1c2fdd9e42b947
wof526 <139619301+wof526@users.noreply.github.com>
2024-03-06 23:14:30 +0900

    [Bronze V] Title: 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    int n;
+    int sum = 0;
+
+    cin >> n;
+    for(int i = 0; i<=n; i++){
+        sum = sum+i;
+    }
+
+    cout << sum;
+
+    return 0;
+
+}

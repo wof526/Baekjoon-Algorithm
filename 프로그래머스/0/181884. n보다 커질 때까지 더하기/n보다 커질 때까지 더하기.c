834e900282e4bec579b24659afb0081b2b583015
Yura Chung <139619301+wof526@users.noreply.github.com>
2024-11-13 21:45:30 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// numbers_len은 배열 numbers의 길이입니다.
+int solution(int numbers[], size_t numbers_len, int n) {
+    int answer = 0;
+    int i=0;
+    
+    while(answer <= n){
+        answer += numbers[i];
+        i++;
+    }
+    return answer;
+}
